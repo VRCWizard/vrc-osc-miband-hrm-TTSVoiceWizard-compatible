f862187404a002c4b047a94bb7f5f778a900acce
VRCWizard <101527472+VRCWizard@users.noreply.github.com>
2022-12-15 13:38:03 -0600

    sends to TTS Voice Wizard

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ const osc = require("osc");
 const WebSocket = require('ws');
 const open = require('open');
 
-const server = new WebSocket.Server({ port: 3228});
+const server = new WebSocket.Server({ port: 3228 });
 
 let vrchatOSC = new osc.UDPPort({
     remoteAddress: "localhost",
-    remotePort: 9000,
+    remotePort: 4026,
     metadata: true
 });
 
@@ -21,32 +21,32 @@ server.on('connection', ws => {
             console.log("Got heart rate: 0 bpm, skipping parameter update...");
         } else {
             console.log('Got heart rate: %s bpm', data);
-            let heartrate = {
-                address: "/avatar/parameters/Heartrate",
-                args:
-                    {
-                        type: "f",
-                        value: data/127-1
-                    }
-            };
-            let heartrate2 = {
-                address: "/avatar/parameters/Heartrate2",
-                args:
-                    {
-                        type: "f",
-                        value: data/255
-                    }
-            };
+            /*  let heartrate = {
+                  address: "/avatar/parameters/Heartrate",
+                  args:
+                      {
+                          type: "f",
+                          value: data/127-1
+                      }
+              };
+              let heartrate2 = {
+                  address: "/avatar/parameters/Heartrate2",
+                  args:
+                      {
+                          type: "f",
+                          value: data/255
+                      }
+              };*/
             let heartrate3 = {
-                address: "/avatar/parameters/Heartrate3",
+                address: "/avatar/parameters/HR",
                 args:
-                    {
-                        type: "i",
-                        value: data
-                    }
+                {
+                    type: "i",
+                    value: data
+                }
             };
-            vrchatOSC.send(heartrate);
-            vrchatOSC.send(heartrate2);
+            // vrchatOSC.send(heartrate);
+            //vrchatOSC.send(heartrate2);
             vrchatOSC.send(heartrate3);
         }
     });
